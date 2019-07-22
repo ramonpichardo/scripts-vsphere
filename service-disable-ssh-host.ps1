d6457fe1102538db5abfc65bfc4f78ec3a48d597
Ramon <ramonpichardo+github@gmail.com>
2019-07-22 14:37:36 -0700

    Create service-disable-ssh-host.ps1

---
+++ service-disable-ssh-host.ps1
@@ -0,0 +1,9 @@
+# PowerCLI - Turn Off SSH Service - Single Host Level
+
+$VIServer = "vcenter1.lab.local"
+$VMHost = "esxi01.lab.local"
+
+Connect-VIServer $VIServer -AllLinked
+
+
+Get-VMHost -name $VMHost | ForEach {Stop-VMHostService -HostService ($_ | Get-VMHostService | Where {$_.Key -eq "TSM-SSH"}) -Confirm:$FALSE}
